770fa0c96d39803ff886d83a35f584eae31ae0a7
Bharadwaja Edera <genuinebharadwaja01.com>
2023-02-23 20:22:58 +0530

    Setup file added

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Demo DVC Project using AIOPS',
+    description='DVC DL-TF practise',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/BharadwajEdera/dvc_DL_TensorFlow_demo_AIOPS",
     author='Bharadwaja Edera',
-    license='',
+    python_requires=">=3.7",
+    install_requires=[
+                'dvc',
+                'matplotlib',
+                'numpy',
+                'pandas',
+                'PyYAML',
+                'tensorflow',
+                'tqdm',
+                'boto3'
+    ]
+
 )
