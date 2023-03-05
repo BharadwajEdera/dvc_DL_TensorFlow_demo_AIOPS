17f3307c7dc122f5255c5bf80d4548e4d21535cd
Bharadwaja Edera <genuinebharadwaja01.com>
2023-03-05 15:37:49 +0530

    Stage_01_load_save Added

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='DVC DL-TF practise',
+    name="src",
+    version="0.0.1",
+    description="A small package for dvc dl pipeline demo",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url = "https://github.com/BharadwajEdera/dvc_DL_TensorFlow_demo_AIOPS",
-    author='Bharadwaja Edera',
+    author="Bharadwaja Edera",
+    packages=["src"],
     python_requires=">=3.7",
     install_requires=[
-                'dvc',
-                'matplotlib',
-                'numpy',
-                'pandas',
-                'PyYAML',
-                'tensorflow',
-                'tqdm',
-                'boto3'
+        "dvc",
+        "tensorflow",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "tqdm",
+        "PyYAML",
+        "boto3" ,
     ]
-
 )
